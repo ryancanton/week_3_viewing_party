117880dcbfe89e3b31f4402d8b96305100e6be70
Ryan Canton <ryancanton999@gmail.com>
2023-02-13 11:25:46 -0700

    Test/Feat: User story 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,21 @@ class UsersController <ApplicationController
         end 
     end 
 
+    def login_form
+
+    end
+
+    def login_user
+        user = User.find_by(email: params[:email])
+        if user.authenticate(params[:password])
+            flash[:success] = "Welcome, #{user.name}!"
+            redirect_to user_path(user)
+        else
+            flash[:error] = "Sorry, your credentials are bad."
+            render :login_form
+        end
+    end
+
     private 
 
     def user_params 
