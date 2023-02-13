861803ac9c90f6b23367f5a3a6c7b4cb2af9d0e6
Ryan Canton <ryancanton999@gmail.com>
2023-02-13 11:10:29 -0700

    Test: Add all sad path tests for user registration

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -27,6 +27,44 @@ RSpec.describe "User Registration" do
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
+  end
+
+  it 'does not create a user if password does not match password confirmation' do
+    visit register_path
     
+    fill_in :user_name, with: 'User Two'
+    fill_in :user_email, with:'meg@test.com'
+    fill_in :user_password, with: 'password123'
+    fill_in :user_password_confirmation, with: 'password1234'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+
+  it 'does not create a user if name is not filled in' do
+    visit register_path
+    
+    fill_in :user_name, with: ''
+    fill_in :user_email, with:'meg@test.com'
+    fill_in :user_password, with: 'password123'
+    fill_in :user_password_confirmation, with: 'password123'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it 'does not create a user if name is not filled in' do
+    visit register_path
+    
+    fill_in :user_name, with: 'Bobby'
+    fill_in :user_email, with:'meg@test.com'
+    fill_in :user_password, with: ''
+    fill_in :user_password_confirmation, with: ''
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password digest can't be blank and Password can't be blank")
   end
 end
